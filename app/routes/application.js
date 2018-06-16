1835c8aaa294d5d01ae3bed85fd4fdb1065b6d12
Chelsea Cianciolo <CCC@Chelseas-MacBook-Air.local>
2018-06-16 15:06:30 +1000

    Fix Indentation

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model(){
-       return this.store.findAll("appreciation");
-    }
+  model() {
+    return this.store.findAll("appreciation");
+  }
 });
